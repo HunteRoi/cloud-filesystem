861584ff3b8b57a626bb526e00569dedb6fcb34b
HunteRoi <me@tinaeldevresse.eu>
2022-05-05 21:53:21 +0200

    Add Serilog support

--- src/CloudFileSystem.WebAPI/Program.cs
+++ src/CloudFileSystem.WebAPI/Program.cs
@@ -1,9 +1,10 @@
+using Serilog;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+builder.Host.UseSerilog((ctx, config) => config.ReadFrom.Configuration(ctx.Configuration));
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
